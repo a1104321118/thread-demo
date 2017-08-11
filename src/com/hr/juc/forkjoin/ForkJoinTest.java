e19dbbee64ac523f27ae3e95e0e4bc7710c8e518
huangrui <1104321118@qq.com>
2017-08-11 09:51:02 +0800

    forkjoin

--- src/com/hr/juc/forkjoin/ForkJoinTest.java
+++ src/com/hr/juc/forkjoin/ForkJoinTest.java
@@ -1,18 +1,30 @@
 package com.hr.juc.forkjoin;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.Future;
-import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.*;
 
 /**
- * Created by hr on 2017/08/10.
+ * fork/join 框架，是一个并行处理框架，在juc包中
+ *
+ * fork：
+ *  将一个大任务细分化，细分到一定程度之后（用户自己设置），加入到若干个任务队列（双向队列）
+ *  每个队列分配一个线程去处理（从队列头开始取）
+ *  假如某一个队列的任务已经被处理完了，但是还有其它队列中还有任务，那么使用工作窃取算法
+ *  工作窃取：该线程去处理其他队列的任务（从队列尾开始取）
+ *
+ * join:
+ *  把fork之后的结果给合并起来
+ *
+ * fork/join 有两个实现类：
+ *  RecursiveTask<E> 对应有返回结果的任务
+ *  RecursiveAction 对应无返回结果的任务
  */
 public class ForkJoinTest {
 
     public static void main(String[] args) {
-        CountTask task = new CountTask(1, 100);
+
+        CountTask task = new CountTask(1, 10);
         Future<Integer> result = new ForkJoinPool().submit(task);
+
         try {
             System.out.println(result.get());
         } catch (InterruptedException e) {
@@ -42,6 +54,11 @@ class CountTask extends RecursiveTask<Integer>{
         if(canCompute){
             for (int i=begin; i<=end; i++){
                 sum += i;
+            }
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }else {
             int middle = (begin + end)/2;
